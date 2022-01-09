01a388933d6bc0d43e4a6684c01e8542a95f7c44
Dmitry <Dmitry.Gorbachev@t-systems.com>
2022-01-09 17:05:20 +0300

    Homework - extracted 3 components out of Card component

--- src/shared/CardsList/Card/Card.tsx
+++ src/shared/CardsList/Card/Card.tsx
@@ -1,30 +1,13 @@
 import React from 'react';
 import styles from './card.css';
+import {TextContent} from './TextContent';
+import {KarmaCounter} from './KarmaCounter';
+import {CommentsButton} from './CommentsButton';
 
 export function Card() {
   return (
     <li className={styles.card}>
-      <div className={styles.textContent}>
-        <div className={styles.metaData}>
-          <div className={styles.userLink}>
-            <img
-              className={styles.avatar}
-              src="https://cdn.dribbble.com/users/1338391/avatars/small/6400fbd7246ce73a2fab2ab9e3b8453b.jpg?1622096188"
-              alt="avatar"
-            />
-            <a href="#user-url" className={styles.username}>Дмитрий Гришин</a>
-          </div>
-          <span className={styles.createdAt}>
-            <span className={styles.publishedLabel}>опубликовано </span>
-              4 часа назад
-          </span>
-        </div>
-        <h2 className={styles.title}>
-          <a href="#post-url" className={styles.postLink}>
-            Следует отметить, что новая модель организационной деятельности Следует...
-          </a>
-        </h2>
-      </div>
+      <TextContent />
       <div className={styles.preview}>
         <img className={styles.previewImg} src="https://cdn.dribbble.com/users/59947/screenshots/17223814/media/6b4fff5aa2bf7e956386cbcfeb5e6513.jpg"/>
       </div>
@@ -38,25 +21,8 @@ export function Card() {
         </button>
       </div>
       <div className={styles.controls}>
-        <div className={styles.karmaCounter}>
-          <button className={styles.up}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="19" height="10" viewBox="0 0 19 10" fill="none">
-              <path d="M9.5 0L0 10H19L9.5 0Z" fill="#D9D9D9"/>
-            </svg>
-          </button>
-          <span className={styles.karmaValue}>234</span>
-          <button className={styles.down}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="19" height="10" viewBox="0 0 19 10" fill="none">
-              <path d="M9.5 10L19 0L8.74228e-07 -1.66103e-06L9.5 10Z" fill="#D9D9D9"/>
-            </svg>
-          </button>
-        </div>
-        <button className={styles.commentsButton}>
-          <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 15 15" fill="none">
-            <path d="M12.75 0H1.41667C0.6375 0 0 0.6375 0 1.41667V9.91667C0 10.6958 0.6375 11.3333 1.41667 11.3333H11.3333L14.1667 14.1667V1.41667C14.1667 0.6375 13.5292 0 12.75 0ZM11.3333 8.5H2.83333V7.08333H11.3333V8.5ZM11.3333 6.375H2.83333V4.95833H11.3333V6.375ZM11.3333 4.25H2.83333V2.83333H11.3333V4.25Z" fill="#C4C4C4"/>
-          </svg>
-          <span className={styles.commentsNumber}>13</span>
-        </button>
+        <KarmaCounter />
+        <CommentsButton />
         <div className={styles.actions}>
           <button className={styles.shareButton}>
             <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
