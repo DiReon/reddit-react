fa001ca0684fe3fe1d63d5916659beca45020fc2
Dmitry Gorbachev <direon16@gmail.com>
2022-04-30 15:45:28 +0300

    Add console.logs for troubleshooting

--- src/shared/Header/SearchBlock/UserBlock/UserBlock.tsx
+++ src/shared/Header/SearchBlock/UserBlock/UserBlock.tsx
@@ -4,32 +4,35 @@ import {EColors, Text} from '../../../Text';
 import {AnonimIcon} from '../../../Icons';
 
 interface IUserBlockProps {
-  avatarSrc?: string;
-  username?: string;
-  loading?: boolean;
+    avatarSrc?: string;
+    username?: string;
+    loading?: boolean;
 }
 
 export function UserBlock({avatarSrc, username, loading}: IUserBlockProps) {
-  return (
-    <a
-      href={`https://www.reddit.com/api/v1/authorize?client_id=${process.env.CLIENT_ID}&response_type=code&state=random_string&redirect_uri=${process.env.REDIRECT_URI}&duration=permanent&scope=read submit identity`}
-      className={styles.userBox}>
-      <div className={styles.avatarBox}>
-        {avatarSrc
-          ? <img src={avatarSrc} alt="User avatar" className={styles.avatarImage} />
-          : <AnonimIcon />
-          // : <Icon name={EIcon.save} size={16}/>
-          // : <MenuIcon />
-          }
-      </div>
+    console.log(process.env);
+    console.log(process.env.CLIENT_ID);
+    console.log(process.env.REDIRECT_URI);
+    return (
+        <a
+            href={`https://www.reddit.com/api/v1/authorize?client_id=${process.env.CLIENT_ID}&response_type=code&state=random_string&redirect_uri=${process.env.REDIRECT_URI}&duration=permanent&scope=read submit identity`}
+            className={styles.userBox}>
+            <div className={styles.avatarBox}>
+                {avatarSrc
+                    ? <img src={avatarSrc} alt="User avatar" className={styles.avatarImage}/>
+                    : <AnonimIcon/>
+                    // : <Icon name={EIcon.save} size={16}/>
+                    // : <MenuIcon />
+                }
+            </div>
 
-      <div className={styles.username}>
-        <br/>
-        {loading ?
-          <Text size={20} color={EColors.black}>Загрузка...</Text> :
-          <Text size={20} color={username ? EColors.black : EColors.gray99}>{username || 'Аноним'}</Text>
-        }
-      </div>
-    </a>
-  );
+            <div className={styles.username}>
+                <br/>
+                {loading ?
+                    <Text size={20} color={EColors.black}>Загрузка...</Text> :
+                    <Text size={20} color={username ? EColors.black : EColors.gray99}>{username || 'Аноним'}</Text>
+                }
+            </div>
+        </a>
+    );
 }
